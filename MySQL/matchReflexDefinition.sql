483f9e4eb08917e3bb2b1d946610be1cf3f8f721
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-10-10 14:13:13 -0400

    lex match query v1.2

--- MySQL/matchReflexDefinition.sql
+++ MySQL/matchReflexDefinition.sql
@@ -6,8 +6,23 @@ SELECT ref.lex_ref_link_id, ref.orig_lang_abbrev, ref.reflex, ls.id, ls.lemma, l
 FROM lex_ref_link ref
 JOIN lewis_short ls 
 -- ON ref.reflex = ls.lemma
-ON ref.reflex = ls.lemma 
-OR ref.reflex LIKE CONCAT(ls.lemma, ',')
+ON REPLACE(
+	REPLACE(
+		REPLACE(ref.reflex, 'v', 'u'),
+			'j', 'i'),
+				'-', '') 
+	= REPLACE(
+		REPLACE(ls.lemma, 'v', 'u'),
+			'j', 'i')
+OR REPLACE(
+	REPLACE(
+		REPLACE(ref.reflex, 'v', 'u'),
+			'j', 'i'),
+				'-', '') 
+	LIKE CONCAT(
+		REPLACE(
+			REPLACE(ls.lemma, 'v', 'u'),
+				'j', 'i'), ',%')
 WHERE ref.orig_lang_abbrev LIKE '%lat%';
 
 SELECT COUNT(*)
@@ -19,5 +34,3 @@ WHERE ref.orig_lang_abbrev LIKE '%lat%';
 SELECT COUNT(*)
 FROM lex_ref_link
 WHERE orig_lang_abbrev='lat.';
-
-SELECT * FROM 
