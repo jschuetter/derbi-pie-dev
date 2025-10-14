6cef16f78ab55399bc035c05d6e63934e0633573
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-10-14 15:57:05 -0400

    Unify comment syntax in .sql file

--- MySQL/matchReflexDefinition-clean.sql
+++ MySQL/matchReflexDefinition-clean.sql
@@ -1,21 +1,21 @@
-# Create matching tables
+-- Create matching tables
 CREATE TABLE reflex_matches (
 	lex_ref_link_id INT PRIMARY KEY NOT NULL,
-    match_str TEXT # null allowed - #46314 has null reflex?
+    match_str TEXT -- null allowed - #46314 has null reflex?
 );
 CREATE TABLE lemma_matches (
 	lewis_short_id INT PRIMARY KEY NOT NULL,
     match_str TEXT NOT NULL
 );
 
-TRUNCATE TABLE reflex_matches;
+-- TRUNCATE TABLE reflex_matches;
 INSERT INTO reflex_matches
 (lex_ref_link_id, match_str, lewis_short_id)
 SELECT lex_ref_link_id, reflex, NULL
 FROM lex_ref_link
 WHERE orig_lang_abbrev LIKE '%lat%';
 
-# Alter match_str in reflex_matches
+-- Alter match_str in reflex_matches
 SELECT * FROM (
 SELECT REGEXP_SUBSTR(REGEXP_REPLACE(match_str, '[-()*]', ''), '(\\w+)') AS col1, match_str, lex_ref_link_id FROM reflex_matches 
 ) AS innertable
@@ -35,14 +35,14 @@ SET SQL_SAFE_UPDATES = 1;
 SELECT * FROM reflex_matches;
 
 
-# Build lemma_matches table
+-- Build lemma_matches table
 TRUNCATE TABLE lemma_matches;
 INSERT INTO lemma_matches
 (lewis_short_id, match_str)
 SELECT id, REGEXP_REPLACE(REPLACE(REPLACE(lemma, 'v', 'u'), 'j', 'i'), '[0-9]', '')  # Replace v/u, j/i, and numerals
 FROM lewis_short;
 
-# New approach: create new table to match reflexes to lemmas
+-- New approach: create new table to match reflexes to lemmas
 CREATE TABLE reflex_lemma_link (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     lex_ref_link_id INT NOT NULL,
@@ -50,8 +50,8 @@ CREATE TABLE reflex_lemma_link (
     lewis_short_id INT
 );
 
-TRUNCATE TABLE reflex_lemma_link;
-# Build mapping table in blocks (10k IDs/1k entries each) to avoid SQL server timeout
+-- TRUNCATE TABLE reflex_lemma_link;
+-- Build mapping table in blocks (10k IDs/1k entries each) to avoid SQL server timeout
 INSERT INTO reflex_lemma_link
 (lex_ref_link_id, match_str, lewis_short_id)
 SELECT rf.lex_ref_link_id, rf.match_str, ls.lewis_short_id
@@ -65,25 +65,25 @@ WHERE rf.lex_ref_link_id > 100000
 ORDER BY rf.lex_ref_link_id;
 -- LIMIT 1000;
 
-# Delete query if need to undo something above
+-- Delete query if need to undo something above
 SET SQL_SAFE_UPDATES = 0;
 DELETE FROM reflex_lemma_link
 WHERE lex_ref_link_id >= 50000;
 SET SQL_SAFE_UPDATES = 1;
 
-# Handle emtpy cells - currently 
-# Lemmas with multiple Lewis & Short entries (i.e. lemmas suffixed with '1' or '2')
-# Now handled in creating lemma_matches table
+-- Handle emtpy cells - currently 
+-- Lemmas with multiple Lewis & Short entries (i.e. lemmas suffixed with '1' or '2')
+-- Now handled in creating lemma_matches table
 -- UPDATE reflex_lemma_link rl
 -- JOIN lemma_matches lm
 -- ON rl.match_str = lm.match_str
 -- SET rl.lewis_short_id = lm.lewis_short_id
 -- WHERE rl.lewis_short_id IS NULL
 -- AND rl.lex_ref_link_id > 10000;
-# Now 973 null entries
+-- Now 973 null entries
 
-# Entries of other principal parts (infinitives or perfect forms)
-# FAILED DON'T DO THIS
+-- Entries of other principal parts (infinitives or perfect forms)
+-- FAILED DON'T DO THIS
 -- CREATE TABLE reflex_lemma_link_orth (
 -- 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 --     lex_ref_link_id INT NOT NULL,
