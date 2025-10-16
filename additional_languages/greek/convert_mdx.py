0905f0c1f8154fb100fe7f23a7ca3e7296a89304
Winjapati <andrewbyrd@uky.edu>
2025-10-16 10:24:44 -0400

    secured xml files for Gk

--- additional_languages/greek/convert_mdx.py
+++
@@ -1,9 +0,0 @@
-from pyglossary.glossary_v2 import Glossary  # v2 API
-
-inp = "LS_gk.mdx"
-out = "LS_gk.xdxf"
-
-g = Glossary()
-g.read(inp, formatName="mdict")                 # <-- not "mdx"
-g.write(out, formatName="xdxf", keep_html=True) # or "tabfile"/"json"
-print("Done →", out)
