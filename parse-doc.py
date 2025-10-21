ea01f4313931335f05693305eeaf8dead894221f
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-10-21 12:14:35 -0400

    Add URL CLI arg to corpus parser

--- parse-doc.py
+++ parse-doc.py
@@ -2,7 +2,9 @@
 parse-doc.py
 03 October 2025
 
-Document parsing script - version 2.
+Document parsing script - version 2
+
+21 Oct 2025 - add parsing from URL
 '''
 
 import cltk
@@ -11,21 +13,50 @@ from cltk.phonology import syllabifier_processes, transcription_processes
 from cltk.dependency.tree import DependencyTree
 
 from time import time
-import csv, os, re
+import csv, os, sys, re
+import urllib.request
 
-INPUT_FILE = "corpus/latin/livy.ab_urbe_condita.part.1.books_1-10.tess"
-OUTPUT_DIR = "corpus/parsed/livy/ab_urbe_condita/"
-OUTPUT_FILE = OUTPUT_DIR + "livy.ab_urbe_condita.part.1.csv"
+OUTPUT_DIR_PFX = "corpus/parsed/"
+
+# Get input file from CLI arg
+if len(sys.argv) < 2: 
+	raise ValueError("Provide an input file as a filepath or URL as the first command-line argument.")
+inputFile = sys.argv[1]
+isUrl = False
+if inputFile.startswith("https://"):
+	isUrl = True
+
+if not inputFile.endswith(".tess"):
+	raise ValueError("This script is designed to work with Tesserae project files. Please provide a compatible file.")
+
+# Parse output file from filename
+filename = inputFile.split("/")[-1]
+author, work, *_ = filename.split(".")
+outputDir = os.path.join(OUTPUT_DIR_PFX, author, work)
+outputFile = os.path.join(outputDir, filename.rstrip(".tess") + ".csv")
+# Create output dir if not exists
+os.makedirs(outputDir, exist_ok=True)
 
 # Read document
-with open(INPUT_FILE) as text:
-	full_text = text.read()
-	# Remove the section in angle brackets at the beginning of each line
-	clean_text = re.sub(r'^\<[ a-zA-Z0-9.]*\>\s', '', full_text, flags=re.MULTILINE)
-	# Test text
-	print(clean_text[:500])
-	# Cut down document length for demo
-	print("Approximate token count:", len(clean_text.split()))
+full_text = None
+if isUrl:
+	with urllib.request.urlopen(inputFile) as res:
+		full_text = res.read().decode('utf-8')
+else:
+	with open(inputFile) as text:
+		full_text = text.read()
+# Test response
+# print(full_text[:500])
+
+# Clean document
+# Remove the section in angle brackets at the beginning of each line
+clean_text = re.sub(r'^\<[ a-zA-Z0-9.]*\>\s', '', full_text, flags=re.MULTILINE)
+# Test text
+# print(clean_text[:500])
+print("Loaded file", filename)
+print("Output path:", outputFile)
+print("Approximate token count:", len(clean_text.split()))
+print()
 
 # Load pipeline for Latin
 cltk_nlp = NLP(language="lat")
@@ -41,8 +72,6 @@ cltk_nlp = NLP(language="lat")
 cltk_nlp.pipeline.processes.remove(cltk.embeddings.processes.LatinEmbeddingsProcess)
 # Remove lexicon process (separate lookup table)
 cltk_nlp.pipeline.processes.remove(cltk.lexicon.processes.LatinLexiconProcess)
-print(cltk_nlp.pipeline.processes)
-print()
 # Add other processes
 # cltk_nlp.pipeline.processes.append(cltk.ner.processes.LatinNERProcess)  # NER unavailable for Latin?
 cltk_nlp.pipeline.processes.append(syllabifier_processes.LatinSyllabificationProcess)
@@ -52,6 +81,7 @@ print("Final pipeline:", cltk_nlp.pipeline.processes)
 
 # Analyze text
 start_time = time()
+print("Parsing...")
 cltk_doc = cltk_nlp.analyze(text=clean_text)
 print(f"Parsing time: {time() - start_time} seconds")
 print("Tokens parsed:", len(cltk_doc.words))
@@ -88,9 +118,8 @@ for w in cltk_doc.words:
 			row[key] = val
 	rows.append(row)
 
-# Create files if not existent
-os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
-with open(OUTPUT_FILE, "w", newline='') as f:
+# Write output
+with open(outputFile, "w", newline='') as f:
 	writer = csv.DictWriter(f, fieldnames=COLUMN_HEADERS)
 	writer.writeheader()  # Write header row
 	writer.writerows(rows)  # Write data rows
