f7eb3bd3ccdfd0482b0d7bb969db990b9a614d48
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-11-01 15:58:10 -0400

    Update lexicon SQL importer

--- MySQL/loadLexCsv.sql
+++ MySQL/loadLexCsv.sql
@@ -1,13 +1,20 @@
 CREATE DATABASE derbi_pie_sql;
 USE derbi_pie_sql;
+DROP TABLE lewis_short;
 CREATE TABLE lewis_short (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	entry_id INT NOT NULL PRIMARY KEY,
+    lemma_id INT NOT NULL,
     lemma TEXT NOT NULL,
+    parent_id INT, 
+    child_ids TEXT,
+    sense_num TEXT, 
+    page_num INT NOT NULL,
     type TEXT NOT NULL,
     orthography TEXT,
     pos TEXT,
     etymology TEXT,
-    entry MEDIUMTEXT
+    entry MEDIUMTEXT,
+    entry_str MEDIUMTEXT
 );
 # Load data from CSV
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/lewis-short.csv'
@@ -16,4 +23,4 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES
-(lemma, type, orthography, pos, etymology, entry);
+(entry_id, lemma_id, lemma, parent_id, child_ids, sense_num, page_num, type, orthography, pos, etymology, entry, entry_str);
