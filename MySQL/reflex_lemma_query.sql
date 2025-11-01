e89a1981df2c5f32b9982c0ec9153957156ff50e
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-11-01 15:57:15 -0400

    Update reflex_lemma_table query

--- MySQL/reflex_lemma_query.sql
+++ MySQL/reflex_lemma_query.sql
@@ -1,7 +1,8 @@
-SELECT ref.reflex, ref.gloss_orig, ls.lemma, ls.entry
+SELECT ref.reflex, ref.gloss_orig, ls.lemma, ls.entry_str
 FROM lex_ref_link ref
 JOIN reflex_lemma_link lnk
 ON ref.lex_ref_link_id = lnk.lex_ref_link_id
 JOIN lewis_short ls
-ON ls.id = lnk.lewis_short_id
+ON ls.lemma_id = lnk.lewis_short_id
+WHERE ls.parent_id IS NULL
 LIMIT 10000;
