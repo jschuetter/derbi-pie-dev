18e44c6c6aafd913a11e24aaac7e9e2f4700fbd2
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-11-05 14:06:35 -0500

    Update reflex_lemma_table with first sense

--- MySQL/reflex_lemma_query.sql
+++ MySQL/reflex_lemma_query.sql
@@ -1,8 +1,11 @@
-SELECT ref.reflex, ref.gloss_orig, ls.lemma, ls.entry_str
+SELECT ref.reflex, ref.gloss_orig, ls.lemma, ls.entry_str, ls2.entry_str
 FROM lex_ref_link ref
 JOIN reflex_lemma_link lnk
 ON ref.lex_ref_link_id = lnk.lex_ref_link_id
 JOIN lewis_short ls
 ON ls.lemma_id = lnk.lewis_short_id
-WHERE ls.parent_id IS NULL
+LEFT JOIN lewis_short ls2
+-- ON SUBSTRING(ls.child_ids, 1, LOCATE(',', ls.child_ids)) = ls2.entry_id
+ON ls.child_ids = ls2.entry_id
+WHERE ls.`type` != 'sense'
 LIMIT 10000;
