aa4b815b6b9ce17ec37d41cf387c62e7a5044fe3
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-11-11 17:57:36 -0500

    Update reflex table with full entries

--- MySQL/exportToCsv.sql
+++ MySQL/exportToCsv.sql
@@ -1,6 +1,19 @@
 SELECT *
-FROM reflex_lemma_link
-INTO OUTFILE 'path/reflex_lemma_link.csv'
+FROM lex_senses
+INTO OUTFILE 'filepath'
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ESCAPED BY '\\'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n' ;
+
+-- Query to file
+SELECT lm.lemma_id, MAX(ms.lemma_orig), lm.entry_str, GROUP_CONCAT(ls.entry_str SEPARATOR '; ')
+FROM lex_master lm
+JOIN master_stg ms ON ms.lemma_id = lm.lemma_id
+LEFT JOIN lex_senses ls ON ls.lemma_id = lm.lemma_id
+GROUP BY lm.lemma_id;
+INTO OUTFILE 'filepath'
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ESCAPED BY '\\'
