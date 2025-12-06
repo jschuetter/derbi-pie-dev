9bf16d956b464147338233565e6e1c33e5037417
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-12-06 11:55:01 -0500

    Fix indenting convention in parse_doc

--- parser/parse_doc.py
+++ parser/parse_doc.py
@@ -19,113 +19,113 @@ import urllib.request
 OUTPUT_DIR_PFX = "corpus/parsed/"
 
 def parse_doc(input_text: str, output_path: str):
-	# Load pipeline for Latin
-	cltk_nlp = NLP(language="lat")
-	# Default: 
-		# 'cltk.alphabet.processes.LatinNormalizeProcess'
-		# 'cltk.dependency.processes.LatinStanzaProcess'
-		# 'cltk.embeddings.processes.LatinEmbeddingsProcess'
-		# 'cltk.stops.processes.StopsProcess'
-		# 'cltk.lexicon.processes.LatinLexiconProcess'
+    # Load pipeline for Latin
+    cltk_nlp = NLP(language="lat")
+    # Default: 
+        # 'cltk.alphabet.processes.LatinNormalizeProcess'
+        # 'cltk.dependency.processes.LatinStanzaProcess'
+        # 'cltk.embeddings.processes.LatinEmbeddingsProcess'
+        # 'cltk.stops.processes.StopsProcess'
+        # 'cltk.lexicon.processes.LatinLexiconProcess'
 
-	# Customize pipeline
-	# Pop embeddings process
-	cltk_nlp.pipeline.processes.remove(cltk.embeddings.processes.LatinEmbeddingsProcess)
-	# Remove lexicon process (separate lookup table)
-	cltk_nlp.pipeline.processes.remove(cltk.lexicon.processes.LatinLexiconProcess)
-	# Add other processes
-	# cltk_nlp.pipeline.processes.append(cltk.ner.processes.LatinNERProcess)  # NER unavailable for Latin?
-	cltk_nlp.pipeline.processes.append(syllabifier_processes.LatinSyllabificationProcess)
-	cltk_nlp.pipeline.processes.append(transcription_processes.LatinPhonologicalTranscriberProcess)
-	cltk_nlp.pipeline.processes.append(cltk.stem.processes.LatinStemmingProcess)
-	print("Final pipeline:", cltk_nlp.pipeline.processes)
+    # Customize pipeline
+    # Pop embeddings process
+    cltk_nlp.pipeline.processes.remove(cltk.embeddings.processes.LatinEmbeddingsProcess)
+    # Remove lexicon process (separate lookup table)
+    cltk_nlp.pipeline.processes.remove(cltk.lexicon.processes.LatinLexiconProcess)
+    # Add other processes
+    # cltk_nlp.pipeline.processes.append(cltk.ner.processes.LatinNERProcess)  # NER unavailable for Latin?
+    cltk_nlp.pipeline.processes.append(syllabifier_processes.LatinSyllabificationProcess)
+    cltk_nlp.pipeline.processes.append(transcription_processes.LatinPhonologicalTranscriberProcess)
+    cltk_nlp.pipeline.processes.append(cltk.stem.processes.LatinStemmingProcess)
+    print("Final pipeline:", cltk_nlp.pipeline.processes)
 
-	# Analyze text
-	start_time = time()
-	print("Parsing...")
-	cltk_doc = cltk_nlp.analyze(text=input_text)
-	print(f"Parsing time: {time() - start_time} seconds")
-	print("Tokens parsed:", len(cltk_doc.words))
-	print("Sentences parsed:", len(cltk_doc.sentences_tokens))
+    # Analyze text
+    start_time = time()
+    print("Parsing...")
+    cltk_doc = cltk_nlp.analyze(text=clean_text)
+    print(f"Parsing time: {time() - start_time} seconds")
+    print("Tokens parsed:", len(cltk_doc.words))
+    print("Sentences parsed:", len(cltk_doc.sentences_tokens))
 
-	# Write Word data to CSV
-	word_start = time()
-	COLUMN_HEADERS = [
-		"index_token",
-		"index_sentence",
-		"string",
-		"pos",
-		"lemma",
-		"stem",
-		"dependency_relation",
-		"governor",
-		"features",
-		"category",
-		"syllables",
-		"phonetic_transcription"
-	]
-	rows = []
-	sqlNull = "\\N"
-	for w in cltk_doc.words:
-		row = {}
-		for key in COLUMN_HEADERS:
-			val = getattr(w, key)
-			if (
-				isinstance(val, cltk.morphology.morphosyntax.MorphosyntacticFeatureBundle) and len(val) == 0 or 
-				val in ["{}", "[]", ""]
-			):
-				row[key] = sqlNull
-			else: 
-				row[key] = val
-		rows.append(row)
+    # Write Word data to CSV
+    word_start = time()
+    COLUMN_HEADERS = [
+        "index_token",
+        "index_sentence",
+        "string",
+        "pos",
+        "lemma",
+        "stem",
+        "dependency_relation",
+        "governor",
+        "features",
+        "category",
+        "syllables",
+        "phonetic_transcription"
+    ]
+    rows = []
+    sqlNull = "\\N"
+    for w in cltk_doc.words:
+        row = {}
+        for key in COLUMN_HEADERS:
+            val = getattr(w, key)
+            if (
+                isinstance(val, cltk.morphology.morphosyntax.MorphosyntacticFeatureBundle) and len(val) == 0 or 
+                val in ["{}", "[]", ""]
+            ):
+                row[key] = sqlNull
+            else: 
+                row[key] = val
+        rows.append(row)
 
-	# Write output
-	with open(output_path, "w", newline='') as f:
-		writer = csv.DictWriter(f, fieldnames=COLUMN_HEADERS)
-		writer.writeheader()  # Write header row
-		writer.writerows(rows)  # Write data rows
-	print(f"CSV write time: {time() - word_start} seconds")
-	print(f"Total runtime: {time() - start_time} seconds")
+    # Write output
+    with open(output_path, "w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=COLUMN_HEADERS)
+        writer.writeheader()  # Write header row
+        writer.writerows(rows)  # Write data rows
+    print(f"CSV write time: {time() - word_start} seconds")
+    print(f"Total runtime: {time() - start_time} seconds")
 
 
 if __name__ == "__main__":
-	# Get input file from CLI arg
-	if len(sys.argv) < 2: 
-		raise ValueError("Provide an input file as a filepath or URL as the first command-line argument.")
-	inputFile = sys.argv[1]
-	isUrl = False
-	if inputFile.startswith("https://"):
-		isUrl = True
+    # Get input file from CLI arg
+    if len(sys.argv) < 2: 
+        raise ValueError("Provide an input file as a filepath or URL as the first command-line argument.")
+    inputFile = sys.argv[1]
+    isUrl = False
+    if inputFile.startswith("https://"):
+        isUrl = True
 
-	if not inputFile.endswith(".tess"):
-		raise ValueError("This script is designed to work with Tesserae project files. Please provide a compatible file.")
+    if not inputFile.endswith(".tess"):
+        raise ValueError("This script is designed to work with Tesserae project files. Please provide a compatible file.")
 
-	# Parse output file from filename
-	filename = inputFile.split("/")[-1]
-	author, work, *_ = filename.split(".")
-	outputDir = os.path.join(OUTPUT_DIR_PFX, author, work)
-	outputFile = os.path.join(outputDir, filename.rstrip(".tess") + ".csv")
-	# Create output dir if not exists
-	os.makedirs(outputDir, exist_ok=True)
+    # Parse output file from filename
+    filename = inputFile.split("/")[-1]
+    author, work, *_ = filename.split(".")
+    outputDir = os.path.join(OUTPUT_DIR_PFX, author, work)
+    outputFile = os.path.join(outputDir, filename.rstrip(".tess") + ".csv")
+    # Create output dir if not exists
+    os.makedirs(outputDir, exist_ok=True)
 
-	# Read document
-	full_text = None
-	if isUrl:
-		with urllib.request.urlopen(inputFile) as res:
-			full_text = res.read().decode('utf-8')
-	else:
-		with open(inputFile) as text:
-			full_text = text.read()
-	# Test response
-	# print(full_text[:500])
+    # Read document
+    full_text = None
+    if isUrl:
+        with urllib.request.urlopen(inputFile) as res:
+            full_text = res.read().decode('utf-8')
+    else:
+        with open(inputFile) as text:
+            full_text = text.read()
+    # Test response
+    # print(full_text[:500])
 
-	# Clean document
-	# Remove the section in angle brackets at the beginning of each line
-	clean_text = re.sub(r'^\<[ a-zA-Z0-9.]*\>\s', '', full_text, flags=re.MULTILINE)
-	# Test text
-	# print(clean_text[:500])
-	print("Loaded file", filename)
-	print("Output path:", outputFile)
-	print("Approximate token count:", len(clean_text.split()))
-	print()
-	parse_doc(clean_text, outputFile)
+    # Clean document
+    # Remove the section in angle brackets at the beginning of each line
+    clean_text = re.sub(r'^\<[ a-zA-Z0-9.]*\>\s', '', full_text, flags=re.MULTILINE)
+    # Test text
+    # print(clean_text[:500])
+    print("Loaded file", filename)
+    print("Output path:", outputFile)
+    print("Approximate token count:", len(clean_text.split()))
+    print()
+    parse_doc(clean_text, outputFile)
