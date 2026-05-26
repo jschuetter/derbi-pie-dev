5b7f87da1b0b6411ea2ef9d75ce159a9b3d30549
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-05-26 15:00:06 -0400

    Separate multiple definitions

--- lexicon/zoega/xmlReader.py
+++ lexicon/zoega/xmlReader.py
@@ -27,18 +27,6 @@ for xml_entry in root.findall(".//entry"):
     try:
         # print("Entry:", entry)
         lemma = xml_entry.get("word")
-        new_entry = {
-            "lemma_id": str(lemma_idx),
-            "lemma": lemma,
-            "sense_num": [],
-            "type": "",
-            "ipa": "",
-            "pos": "",
-            "gender": "",
-            "entry": "",
-            "entry_str": "",  # Plaintext of entry (without HTML tags)
-            "gloss": "",
-        }
 
         # Split entry if multiple definitions
         # Def'n delimited by `<m1><b>I)</b></m1>` 
@@ -47,12 +35,15 @@ for xml_entry in root.findall(".//entry"):
         # List of definitions
         # Each element contains list of tags belonging to that definition
         defn_delimiters = xml_entry.findall('.//m1[b]')
-        # Print all lemmas & delimiters
-        # print(lemma, *["".join(delim.itertext()) for delim in defn_delimiters])
-        # DEV: make sure all delim matches contain delimiter text
-        # (can't use contains() filter in lxml findall() method)
-        for delim in defn_delimiters: 
-            assert "I)" in "".join(delim.find("./b").itertext())
+        # Print all delimiters
+        # if len(defn_delimiters) > 0: 
+        #     print(lemma, *["".join(delim.itertext()) for delim in defn_delimiters])
+        for delim in defn_delimiters:
+            delim_text = "".join(delim.find("./b").itertext())
+            # Remove delimiters that do not define separate definitions
+            # (can't use contains() filter in lxml findall() method)
+            if not ("I)" in delim_text or "V)" in delim_text): 
+                defn_delimiters.remove(delim)
             
         # Use delimiters to split definitions
         entry_definitions = []
@@ -73,35 +64,61 @@ for xml_entry in root.findall(".//entry"):
             entry_definitions.append(
                 xml_entry[xml_entry.index(defn_delimiters[defn_idx]):]
             )
-            print(lemma, "\n===")
-            print(*["\n".join("".join(elem.itertext()) for elem in defn) for defn in entry_definitions], sep="\n---\n")
-            print()
+            # print(lemma, "\n===")
+            # print(*["\n".join("".join(elem.itertext()) for elem in defn) for defn in entry_definitions], sep="\n---\n")
+            # print()
+
+            # Process each definition separately
+            for entry_idx in range(len(entry_definitions)): 
+                # Create temporary subentry object to hold elements of a single definition
+                defn = etree.Element("subentry")
+                defn.extend(entry_definitions[entry_idx])
+
+                defn_num = []
+                if len(entry_definitions) > 1: 
+                    # Format entry number to match Lewis-Short format
+                    # (use brackets for multiple definitions)
+                    defn_num.append(f"[{entry_idx}]") 
+
+                new_entry = {
+                    "lemma_id": str(lemma_idx),
+                    "lemma": lemma,
+                    "sense_num": defn_num,
+                    "type": "main",  # Main definition
+                    "ipa": "",
+                    "pos": "",
+                    "gender": "",
+                    "entry": "",
+                    "entry_str": "",  # Plaintext of entry (without HTML tags)
+                    "gloss": "",
+                }
+
+                # Get POS or gender, as applicable
+                # Select first <p> tag in entry
+                p_tag = defn.find(".//p")
+                if p_tag in ["m.", "f.", "n."]: 
+                    new_entry["gender"] = p_tag
+                    new_entry["pos"] = "n."
+                elif p_tag == "a.": 
+                    # Normalize 'a.' notation to 'adj.'
+                    new_entry["pos"] = "adj."
+                else: 
+                    # Other tags: adj., adv., pp., prep., interj.
+                    new_entry["pos"] = p_tag
+
+                # TODO: get IPA
+
+                # TODO: get entry senses
+
+                # TODO: create XSLT to make HTML-formatted entry
+
+                # TODO: create gloss 
+                # (concatenate senses or just use first?)
 
 
-        # TODO: GET TYPE
-
-        # Get POS or gender, as applicable
-        # Select first <p> tag in entry
-        p_tag = xml_entry.find(".//p")
-        if p_tag in ["m.", "f.", "n."]: 
-            new_entry["gender"] = p_tag
-            new_entry["pos"] = "n."
-        elif p_tag == "a.": 
-            # Normalize 'a.' notation to 'adj.'
-            new_entry["pos"] = "adj."
-        else: 
-            # Other tags: adj., adv., pp., prep., interj.
-            new_entry["pos"] = p_tag
-
-        # Get entry contents
-        # Default case: one sense only
-        # Case 1: multiple senses
-        # Case 2: multiple definitions
-            # Def'n delimited by `<m1><b>I)</b></m1>`
-        # Case 3: multiple definitions + multiple senses
-
-
-        
+    except AssertionError as ae: 
+        print(f"Assertion failed in entry {lemma}")
     except Exception as e: 
         # lemma = xml_entry.get("entry")
         print(f"Exception in entry {lemma}: {e}")
+        raise e
