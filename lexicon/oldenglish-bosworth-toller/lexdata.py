9c749716b882a693267566fb78da12fc2183b8fa
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-06-08 15:56:38 -0400

    Add abbrev. to lexdata

--- lexicon/oldenglish-bosworth-toller/lexdata.py
+++ lexicon/oldenglish-bosworth-toller/lexdata.py
@@ -5,6 +5,72 @@ Script for running data through CLTK to get additional lexicon fields
 
 from cltk.phonology.ang.phonology import OldEnglishTranscription
 
+# Abbreviations used in orthography notation
+ORTH = [
+    # Verbs
+    "pl.", #  (plural present ending)
+    "p.", #  (preterite singular form)
+    "pp.", #  (past participle form)
+    "pp. of",
+    "part.", #  (present participle)
+    "part. of",
+    "subj.", #  (???)
+    "impert.", #  (imperative)
+    "sing. impert. of",
+
+    # Nouns
+    "gen.",
+    "dat.",
+    "acc.",
+    "abl.",
+    "pl.",
+    "pl. n. acc.", # For some reason
+]
+
+# Valid POS abbreviations
+POS = [
+    "v. trans.",
+    "v. intrans.",
+    "v. a.",
+    "v. n.",
+    "v. reflex.",
+    "v. pers. and impers.",
+    "adj.",
+    "adv.",
+    "adj. pron.",
+    
+    # Prepositions
+    "prep.",
+    "prep. dat.",
+    "prep. c. dat.",
+    "prep. with dat.",
+    "prep. cum dat. inst. acc.",
+    "prep. with dat. acc. inst.",
+    "prep. acc.",
+]
+# POS abbreviations which imply v.
+POS_IMPLIES_V = [
+    "trans.",
+    "intrans."
+]
+# Gender abbreviations (imply n.)
+POS_IMPLIES_N = [
+    "m.",
+    "f.",
+    "n.",
+    "m. n.",
+]
+# Only valid when gloss included with POS abbrev. in <I>
+# cf. 'Ingwine', 'Indéas'
+POS_W_GLOSS = [
+    # Number/Gender - imply n.
+    "pl.",
+    "pl. f.",
+    "pl. m.",
+    "pl. n.",
+]
+POS_ALL = POS + POS_IMPLIES_N + POS_IMPLIES_V + POS_W_GLOSS
+
 def ipa_oldenglish(input_orth): 
     '''
     Return the IPA transcription of the given orthography
