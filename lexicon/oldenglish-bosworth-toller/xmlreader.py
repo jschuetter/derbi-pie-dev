8efd7185dce1fd05f4b0e84736cbd6989d33ec10
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-06-11 10:55:01 -0400

    Parse multiple senses

--- lexicon/oldenglish-bosworth-toller/xmlreader.py
+++ lexicon/oldenglish-bosworth-toller/xmlreader.py
@@ -3,7 +3,7 @@ xmlreader.py
 XML parser script for the Bosworth and Toller Old English dictionary
 '''
 
-import csv
+import csv, re
 from lxml import etree
 from time import time
 import traceback
@@ -11,6 +11,8 @@ import traceback
 import lexdata
 from lexdata import ipa_oldenglish
 from unescape import unescape
+
+SQL_NULL = "\\N"
 
 def line_xml(raw_line, wrapper_tag = "xml_line"):
     '''
@@ -60,7 +62,7 @@ def get_entries(filename):
                 # Extract page number for entries
                 page_num = int(line[12:16])
                 # DEV: stop after page 5
-                if (page_num > 5): 
+                if (page_num > 10): 
                     break
                 print("Page", page_num)
                 continue
@@ -271,55 +273,61 @@ def get_entries(filename):
                             etymology = etymology[:bracket_idx+1]
                             assert remaining == ""
                 
-                # TODO: add check for multiple senses
-                # TODO: check for add'l POS in sense?  => multiple entries, instead of multiple senses?
-
-                # Parse entry & gloss
-                if entry == "":
-                    if remaining:
-                        entry += remaining
-                        print(f"LEMMA {lemma} + REMAINING {remaining}")
-                    
-                    if subtag_idx < len(line_elem):
-                        subtag_text = line_elem[subtag_idx].text or ""
-                        subtag_text_words = subtag_text.split()
-                        # Entry case 1: gloss included in <I> with POS
-                        # Find longest matching substring
-                        if subtag_text_words[0] in lexdata.POS_REMOVE:
-                            assert gloss == ""
-                            
-                            word_idx = 0
-                            while ( " ".join(subtag_text_words[:word_idx+1]) in lexdata.POS_REMOVE and 
-                                word_idx <= len(subtag_text_words) ):
-
-                                word_idx += 1
-                            gloss_text = " ".join(subtag_text_words[word_idx+1:])
-                            if gloss_text.strip() != "":
-                                gloss = gloss_text
-                                assert entry == ""
-                                entry = f"<I>{gloss}</I>"
-                                entry += line_elem[subtag_idx].tail or ""
-                                entry_str = gloss
-                                entry_str += line_elem[subtag_idx].tail or ""
-                                subtag_idx += 1
-
-                    # Parse remaining data
-                    # Case 3: no remaining child tags; entry is remaining tail text
-                    if subtag_idx >= len(line_elem) and entry == "": 
-                        print("PARSE REMAINING")
-                        entry = line_elem[-1].tail
-                        entry_str = line_elem[-1].tail
+                entry_senses = []
+                if subtag_idx < len(line_elem):
+                    # Check for multiple senses
+                    if ( line_elem[subtag_idx].tag == "B" and 
+                        re.match(r'^[IVX][IVX]?I?I?\.', line_elem[subtag_idx].text) ):
+                        entry_senses = parse_senses(line_elem, subtag_idx, {"lemma_idx": lemma_idx, "lemma": lemma, "page_num": page_num})
                         raise EntryCompleted
-                    
-                    # Case 2: gloss in isolated tag (and not yet parsed)
-                    if gloss == "" and line_elem[subtag_idx].tag == "I":
-                        gloss = line_elem[subtag_idx].text
-                    while subtag_idx < len(line_elem): 
-                        subtag = line_elem[subtag_idx]
-                        entry += etree.tostring(subtag, encoding="Unicode")
-                        entry_str += "".join(subtag.itertext()) + (subtag.tail or "")
-                        subtag_idx += 1
-                    raise EntryCompleted
+
+                    else: 
+                        # Parse (single) entry & gloss
+                        if entry == "":
+                            if remaining:
+                                entry += remaining
+                                print(f"LEMMA {lemma} + REMAINING {remaining}")
+                            
+                            if subtag_idx < len(line_elem):
+                                subtag_text = line_elem[subtag_idx].text or ""
+                                subtag_text_words = subtag_text.split()
+                                # Entry case 1: gloss included in <I> with POS
+                                # Find longest matching substring
+                                if subtag_text_words[0] in lexdata.POS_REMOVE:
+                                    assert gloss == ""
+                                    
+                                    word_idx = 0
+                                    while ( " ".join(subtag_text_words[:word_idx+1]) in lexdata.POS_REMOVE and 
+                                        word_idx <= len(subtag_text_words) ):
+
+                                        word_idx += 1
+                                    gloss_text = " ".join(subtag_text_words[word_idx+1:])
+                                    if gloss_text.strip() != "":
+                                        gloss = gloss_text
+                                        assert entry == ""
+                                        entry = f"<I>{gloss}</I>"
+                                        entry += line_elem[subtag_idx].tail or ""
+                                        entry_str = gloss
+                                        entry_str += line_elem[subtag_idx].tail or ""
+                                        subtag_idx += 1
+
+                            # Parse remaining data
+                            # Case 3: no remaining child tags; entry is remaining tail text
+                            if subtag_idx >= len(line_elem) and entry == "": 
+                                print("PARSE REMAINING")
+                                entry = line_elem[-1].tail
+                                entry_str = line_elem[-1].tail
+                                raise EntryCompleted
+                            
+                            # Case 2: gloss in isolated tag (and not yet parsed)
+                            if gloss == "" and line_elem[subtag_idx].tag == "I":
+                                gloss = line_elem[subtag_idx].text
+                            while subtag_idx < len(line_elem): 
+                                subtag = line_elem[subtag_idx]
+                                entry += etree.tostring(subtag, encoding="Unicode")
+                                entry_str += "".join(subtag.itertext()) + (subtag.tail or "")
+                                subtag_idx += 1
+                            raise EntryCompleted
 
             except IndexError as ie: 
                 print(f"IndexError in lemma {lemma}: {ie}")
@@ -347,7 +355,7 @@ def get_entries(filename):
                 save_csv(dict_entries, "bosworth-toller-error.csv")
                 raise e  # Fail loudly
 
-            # Try to impute POS if missing
+            # TODO: Try to impute POS if missing
             # Check gloss (e.g. begins with "To _" or "A/an _")
             # or orthography (pp. --> verb, dat. --> noun)
 
@@ -359,29 +367,122 @@ def get_entries(filename):
             entry = f'<div class="oldenglish bodytext">{entry.strip()}</div>'
             entry_str = entry_str.strip()
 
-            new_entry = {
-                "lemma_id": str(lemma_idx),
-                "lemma": lemma,
-                "sense_num": [],
-                "page_num": str(page_num),
-                "type": "main",
-                "ipa": ipa,
-                "orth": orthography,
-                "pos": pos or "NONE",
-                "gender": gender,
-                "etym": etymology,
-                "entry": entry,
-                "entry_str": entry_str,  # Plaintext of entry (without XML tags)
-                "gloss": gloss,
-            }
-            
+            if not entry_senses:
+                # Single entry sense
+                new_entry = {
+                    "lemma_id": str(lemma_idx),
+                    "lemma": lemma,
+                    "sense_num": "",
+                    "page_num": str(page_num),
+                    "type": "main",
+                    "ipa": ipa,
+                    "orth": orthography,
+                    "pos": pos,
+                    "gender": gender,
+                    "etym": etymology,
+                    "entry": entry,
+                    "entry_str": entry_str,  # Plaintext of entry (without XML tags)
+                    "gloss": gloss,
+                }
+            else: 
+                # Multiple entry senses
+                # Take main entry from first sense
+                first_sense = entry_senses.pop(0)
+                new_entry = {
+                    "lemma_id": str(lemma_idx),
+                    "lemma": lemma,
+                    "sense_num": first_sense["sense_num"],
+                    "page_num": str(page_num),
+                    "type": "main",
+                    "ipa": ipa,
+                    "orth": orthography,
+                    "pos": pos,
+                    "gender": gender,
+                    "etym": etymology,
+                    "entry": first_sense["entry"],
+                    "entry_str": first_sense["entry_str"],  # Plaintext of entry (without XML tags)
+                    "gloss": first_sense["gloss"],
+                }
+
+            new_entries = [ new_entry ] + entry_senses
+
             # Set prev_entry
-            # Append new entry to dict_entries
             if dict_entries: 
-                prev_entry = dict_entries[-1]
-            dict_entries.append(new_entry)
-            
+                prev_entry = new_entry
+
+            # Handle empty fields, append to dict
+            for entry in new_entries:
+                for k,v in entry.items():
+                    if v == "":
+                        entry[k] = SQL_NULL
+                dict_entries.append(entry)
+                
     return dict_entries
+
+def parse_senses(line_elem, subtag_idx, lemma_info):
+    '''
+    Parse and return a list of senses in the provided xml_line element
+    Assumes subtag_idx points to a <B> tag in line_elem that begins
+    the first dictionary sense.
+    Lemma_info is a dict containing lemma_idx, lemma, and page_num 
+    from main entry
+
+    Returns a list of sense objects, matching the schema for other 
+    dictionary entries above. 
+    '''
+    entry_senses = []
+    if line_elem[subtag_idx].tag != "B": 
+        raise ValueError("Init. subtag_idx does not point to <B> tag! (in parse_senses)")
+    if not re.match(r'^[IVX][IVX]?I?I?\.', line_elem[subtag_idx].text):
+        raise ValueError("Init. <B> tag does not contain sense_num")
+
+    while subtag_idx < len(line_elem):
+        # Parse out the rest of the line
+        new_sense = {
+            "lemma_id": str(lemma_info["lemma_idx"]),
+            "lemma": lemma_info["lemma"],
+            "sense_num": "",  # Filled in below
+            "page_num": str(lemma_info["page_num"]),
+            "type": "sense",
+            # Unique fields
+            "entry": "",
+            "entry_str": "",
+            "gloss": "",
+            # Intentionally left blank
+            "ipa": "",
+            "orth": "",
+            "pos": "",
+            "gender": "",
+            "etym": "",
+        }
+        if not re.match(r'^[IVX][IVX]?I?I?\.', line_elem[subtag_idx].text):
+            raise ValueError(f"<B> tag does not contain sense_num. Text contents: {line_elem[subtag_idx].text}")
+        new_sense["sense_num"] = line_elem[subtag_idx].text.strip(".")
+
+        if line_elem[subtag_idx].tail is not None and line_elem[subtag_idx].tail.strip() != "": 
+            print(new_sense["lemma"], new_sense["sense_num"])
+            raise ValueError(f"Sense_num tag tail is nonnull. Contents: {line_elem[subtag_idx].tail}")
+
+        subtag_idx += 1
+        if line_elem[subtag_idx].tag != "I":
+            print(new_sense["lemma"], new_sense["sense_num"])
+            raise ValueError(f"Gloss not found for sense; first tag was not <I>. Tag: {"".join(line_elem[subtag_idx].itertext())}")
+
+        # Parse gloss, entry
+        new_sense["gloss"] = line_elem[subtag_idx].text
+        new_sense["entry"] += "".join(line_elem[subtag_idx].itertext()) + (line_elem[subtag_idx].tail or "")
+        new_sense["entry_str"] += line_elem[subtag_idx].text + (line_elem[subtag_idx].tail or "")
+        subtag_idx += 1
+
+        while subtag_idx < len(line_elem) and line_elem[subtag_idx].tag != "B":
+            # Parse rest of sense
+            new_sense["entry"] += "".join(line_elem[subtag_idx].itertext()) + (line_elem[subtag_idx].tail or "")
+            new_sense["entry_str"] += line_elem[subtag_idx].text + (line_elem[subtag_idx].tail or "")
+            subtag_idx += 1
+
+        entry_senses.append(new_sense)
+
+    return entry_senses
 
 def save_csv(data, filename):
     fieldnames = ["lemma_id", "lemma", "sense_num", "page_num", "type", "ipa", "orthography", "pos", "gender", "etymology", "entry", "entry_str", "gloss"]
