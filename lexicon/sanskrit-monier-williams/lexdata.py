1928041fa842d66e02939c1f806000d5140ef83f
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-06-25 12:09:40 -0400

    Refine transliteration regexp

--- lexicon/sanskrit-monier-williams/lexdata.py
+++ lexicon/sanskrit-monier-williams/lexdata.py
@@ -8,18 +8,23 @@ N.B. CLTK lacks a Sanskrit IPA transcription module
 import re
 from indic_transliteration.sanscript import transliterate, SLP1, IAST, DEVANAGARI
 
-IAST_REGEXP = r'[A-Za-z\u0100-\u017F\u1E00-\u1EFF\u00f1\u0301\u0302 —\'\-]+'
-DEVA_REGEXP = r'[\u0900-\u097F\u0980-\u09FF\uA8E0-\uA8FF —\-]+'
+# Regexp, also including U+221A (√), space, hyphen, dash, combining accents
+IAST_REGEXP = r'[A-Za-z\u0100-\u017F\u1E00-\u1EFF\u00f1\u0300-\u0302\u0306\u221a +—\'\-;()\[\],!\.‘’=~?]+'
+DEVA_REGEXP = r'[\u0900-\u097F\u0980-\u09FF\uA8E0-\uA8FF\u221a +—\-;()\[\],!‘’=~?]+'
 
 def slp1_to_deva(input_data): 
     '''
     Return the Devanagari transliteration of the given
     data in SLP1 format
     '''
-    # Drop "/", indicating udatta diacritic (cannot be transcribed with other diacritics; not in original text)
-    deva_translit = transliterate(input_data.replace("/", ""), SLP1, DEVANAGARI)
-    if re.fullmatch(DEVA_REGEXP, deva_translit) is None: 
-        raise ValueError(f"Return value does not match regexp: {deva_translit} | {[hex(ord(c)) for c in deva_translit]}")
+    if input_data == "":
+        return ""
+    
+    # Drop "/" and "^" (accent transcriptions)
+    # and circumflex accent (<srs/>), and superscript 'o' (prefix marker)
+    deva_translit = transliterate(re.sub(r'[/\^\u0302\u00b0]', '', input_data), SLP1, DEVANAGARI)
+    # if re.fullmatch(DEVA_REGEXP, deva_translit) is None: 
+    #     print(f"Return value does not match regexp: {deva_translit} | {[hex(ord(c)) for c in deva_translit]}")
     return deva_translit
 
 def slp1_to_iast(input_data): 
@@ -27,9 +32,17 @@ def slp1_to_iast(input_data):
     Return the IAST transliteration of the given
     data in SLP1 format
     '''
+    if input_data == "":
+        return ""
+    
     base_translit = transliterate(input_data, SLP1, IAST)
-    # Use combining acute to indicate udatta accent
+    # Use combining acute to indicate udatta accent,
+    # Transcribe '^' as grave accent
+    # ignore superscript 'o' (see above)
     full_translit = base_translit.replace("/", "\u0301")
-    if re.fullmatch(IAST_REGEXP, full_translit) is None: 
-        raise ValueError(f"Return value does not match regexp: {full_translit} | {[hex(ord(c)) for c in full_translit]}")
+    full_translit = full_translit.replace("^", "\u0300")
+    full_translit = full_translit.replace("\u00b0", "")
+    full_translit = full_translit.replace("|", ".")
+    # if re.fullmatch(IAST_REGEXP, full_translit) is None: 
+    #     print(f"Return value does not match regexp: {full_translit} | {[hex(ord(c)) for c in full_translit]}")
     return full_translit
