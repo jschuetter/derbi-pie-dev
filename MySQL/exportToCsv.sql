bd10331786a0f504de3939d0ae23b5007a930869
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-07-03 11:50:28 -0400

    Fix line endings on export CSVs

--- MySQL/exportToCsv.sql
+++ MySQL/exportToCsv.sql
@@ -1,28 +1,26 @@
 -- Query column names
 SELECT GROUP_CONCAT(column_name ORDER BY ordinal_position)
 FROM information_schema.columns
-WHERE table_name = 'skt_multiple_matches'
+WHERE table_name = 'temp_skt_joined'
 AND table_schema = 'derbi_pie_jacob';
 
 SELECT *
-FROM skt_no_matches
+FROM skt_approved_matches
 ORDER BY CAST(REPLACE(parsed_id, '*', '') AS UNSIGNED)
-INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/exports/skt_no_matches.csv'
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/exports/skt_approved_matches_v1.csv'
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ESCAPED BY '\\'
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n' ;
+LINES TERMINATED BY '\r\n' ;
 
 -- Query to file
-SELECT lm.lemma_id, MAX(ms.lemma_orig), lm.entry_str, GROUP_CONCAT(ls.entry_str SEPARATOR '; ')
-FROM lex_master lm
-JOIN master_stg ms ON ms.lemma_id = lm.lemma_id
-LEFT JOIN lex_senses ls ON ls.lemma_id = lm.lemma_id
-GROUP BY lm.lemma_id
-INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/exports/lex_all_entries.csv'
+SELECT parsed_id, master_id
+FROM skt_approved_matches
+ORDER BY CAST(REPLACE(parsed_id, '*', '') AS UNSIGNED)
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/exports/skt_approved_matches.csv'
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ESCAPED BY '\\'
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n' ;
+LINES TERMINATED BY '\r\n' ;
