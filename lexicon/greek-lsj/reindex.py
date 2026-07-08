40833c0cf67f7a2e1532cb29f822fe636db9c915
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-07-08 16:08:07 -0400

    Reindex script with dev comments

--- lexicon/greek-lsj/reindex.py
+++ lexicon/greek-lsj/reindex.py
@@ -2,14 +2,21 @@
 reindex.py
 
 A simple script designed to reindex entries from 
-`grc.lsj.perseus-eng2.csv` onward to handle skipped
-ID at n20213 (skips n20214) -- will unify with 
-existing lex_master schema
+`grc.lsj.perseus-eng2.csv` onward to handle skipped IDs
+in Perseus CSVs
+==Missing IDs:==
+n20214 (CSV 2)
+n77575 (CSV 17)
+n78758 (CSV 17)
+n98366 (CSV 21)
+n99905 (CSV 21)
+
+Will unify with existing lex_master schema.
 '''
 
 import csv
 
-for csv_num in range(2, 28): 
+for csv_num in range(21, 28): 
     filename = f'grc.lsj.perseus-eng{csv_num}.csv'
     # Read rows from CSV
     with open(filename, 'r') as readfile: 
@@ -18,8 +25,18 @@ for csv_num in range(2, 28):
     # Iterate over rows, incrementing lemma_id as necessary
     for row in rows: 
         id = int(row["lemma_id"])
-        if id > 20214: 
-            row["lemma_id"] = id - 1
+        # if id > 99905: 
+        if id >= 99903: # DEV
+            row["lemma_id"] = id - 1 
+        # if id > 98366: 
+        # if id >= 98365: # DEV
+        #     row["lemma_id"] = id - 1 
+        # if id > 78758: 
+        #     row["lemma_id"] = id - 1 
+        # if id > 77575: 
+        #     row["lemma_id"] = id - 1 
+        # if id > 20214:
+        #      row["lemma_id"] = id - 1
 
     # Write back to CSV
     with open(filename, 'w') as writefile: 
